46050e84724b4cabca0f8e90bfa7ce4c44b4ca11
donghun <hoonyhoeny@gmail.com>
2021-10-07 23:35:44 +0900

    날씨 API 사용방법, Geolocation 사용방법

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,43 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
+
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getData();
+  }
+    void getLocation() async{
+    Location location = Location();
+    location.getCurrentLocation();
+  }
+
+    void getData() async {
+    Response response = await get('http://api.openweathermap.org/data/2.5/');
+
+    if (response.statusCode ==200) {
+      String data =response.body;
+    }else {
+      print(response.statusCode);
+    }
+
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+
     );
   }
 }
