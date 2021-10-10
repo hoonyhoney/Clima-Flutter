9f06955566c623e7dc50919a3a0163ccce370bad
donghun <hoonyhoeny@gmail.com>
2021-10-10 22:40:04 +0900

    navigator. push와 pop을 활용한 데이터 전달 방법

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,43 +1,54 @@
-import 'package:clima/services/location.dart';
+
+
+import 'dart:convert';
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart'as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'location_screen.dart';
+
+
 
 class LoadingScreen extends StatefulWidget {
-
 
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
+
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  double latitude;
+  double longitude;
 
   @override
   void initState() {
     super.initState();
-    getLocation();
-    getData();
+    getLocationData();
   }
-    void getLocation() async{
-    Location location = Location();
-    location.getCurrentLocation();
+    void getLocationData() async{
+
+      var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+
   }
 
-    void getData() async {
-    Response response = await get('http://api.openweathermap.org/data/2.5/');
-
-    if (response.statusCode ==200) {
-      String data =response.body;
-    }else {
-      print(response.statusCode);
-    }
-
-    }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body:Center(
+        child:SpinKitDoubleBounce(
+            color: Colors.white,
+            size: 100.0,
+        ),
+      ),
     );
   }
 }
